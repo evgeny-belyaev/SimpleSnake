32f7dbe6b14ea7710d04611b34c308bc5582a22b
evgeny-belyaev <evgeny.belyaev@gmail.com>
2013-05-12 17:27:48 +0400

    --

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-define('app', ['underscore', 'paper', 'install'], function () {
+define('app', ['underscore', 'paper'], function () {
 
 	var canvas = document.getElementById('canvas');
 	paper.setup(canvas);
